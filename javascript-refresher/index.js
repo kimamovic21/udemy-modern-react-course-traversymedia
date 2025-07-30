466282d9e0c8d4947a6aa7a9474b94cab437eb1d
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-30 15:36:28 +0200

    Template literals

--- javascript-refresher/index.js
+++ javascript-refresher/index.js
@@ -1,35 +1,17 @@
-function getRectangleArea(width, height) {
-  return width * height;
+const username = 'John';
+
+// const greeting = 'Hello, ' + name;
+const greeting = `Hello, ${username}`;
+
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  // return date.toLocaleDateString() + ' at ' + date.toLocaleTimeString();
+  return `${date.toLocaleDateString()} at ${date.toLocaleTimeString()}`;
 };
 
-const getRectangleArea = (width, height) => {
-  return width * height;
+const note = {
+  title: 'Discuss project roadmap',
+  timestamp: Date.now(),
 };
 
-const getRectangleArea = (width, height) => width * height;
-
-const sayHello = () => console.log('Hello!');
-
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((number) => number * 2);
-
-regular();
-arrow();
-
-function regular() {
-  console.log('Regular');
-};
-
-const arrow = () => console.log('arrow');
-
-const person = {
-  name: 'Brad',
-  sayHelloRegular: function () {
-    console.log('Regular:', this.name);
-    console.log(this);
-  },
-  sayHelloArrow: () => {
-    console.log('Arrow:', this.name);
-    console.log(this);
-  },
-};
+console.log(`Last edited: ${formatDate(note.timestamp)}`);
