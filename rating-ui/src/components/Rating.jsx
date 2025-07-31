7e12d1ac0e753f5b8134248a4d95181280850afd
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-31 12:23:39 +0200

    Handling events

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,12 +1,14 @@
 const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
+  const clicked = () => console.log('clicked');
+
   return (
     <div className='rating-container'>
       <h2>Rate Your Experience</h2>
       <div className='stars'>
         {stars.map((star) => (
-          <span key={star} className='star'>
+          <span key={star} className='star' onClick={clicked}>
             {'\u2605'}
           </span>
         ))}
