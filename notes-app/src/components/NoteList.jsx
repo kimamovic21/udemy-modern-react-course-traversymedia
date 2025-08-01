06d81d36d4721fa3d149ddf696d59bb48e4262f1
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-01 15:32:33 +0200

    Prop drilling

--- notes-app/src/components/NoteList.jsx
+++ notes-app/src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from './Note';
+
 const NoteList = ({ notes, deleteNote }) => {
   return (
     <div className='space-y-4'>
@@ -5,34 +7,11 @@ const NoteList = ({ notes, deleteNote }) => {
         <p className='text-center text-gray-500'>No notes yet!</p>
       ) : null}
       {notes.map((note) => (
-        <div
+        <Note
           key={note.id}
-          className='p-4 bg-white rounded-lg shadow-md border-l-4'
-          style={{
-            borderLeftColor:
-              note.priority === 'High'
-                ? 'red'
-                : note.priority === 'Medium'
-                  ? 'orange'
-                  : 'green',
-          }}
-        >
-          <h3 className='text-lg font-bold'>{note.title}</h3>
-          <p className='text-sm text-gray-600'>
-            <strong>Category:</strong> {note.category}
-          </p>
-          <p className='text-sm text-gray-600'>
-            <strong>Priority:</strong> {note.priority}
-          </p>
-          <p className='mt-2'>{note.description}</p>
-
-          <button
-            className='mt-3 text-red-500 hover:text-red-700 cursor-pointer transition'
-            onClick={() => deleteNote(note.id)}
-          >
-            🗑 Delete
-          </button>
-        </div>
+          note={note}
+          deleteNote={deleteNote}
+        />
       ))}
     </div>
   );
