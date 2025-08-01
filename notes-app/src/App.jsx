acd810594a9fb750a4e7c9a72705efd46f788303
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-01 12:23:55 +0200

    Show notes

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -14,6 +15,8 @@ const App = () => {
         notes={notes}
         setNotes={setNotes}
       />
+
+      <NoteList notes={notes} />
     </div>
   );
 };
