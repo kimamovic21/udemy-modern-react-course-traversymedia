3311e76efc2f66505fe198a69715840ec11a7a1d
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-02 14:20:25 +0200

    Persist timer across re-renders

--- simple-timer/src/App.jsx
+++ simple-timer/src/App.jsx
@@ -1,32 +1,51 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 const App = () => {
-  const inputRef = useRef(null);
-  console.log(inputRef);
+  const timerRef = useRef(null);
+  console.log(timerRef);
 
-  const submit = () => {
-    console.log(inputRef.current);
-    console.log(inputRef.current.value);
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    };
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null;
   };
 
   return (
     <div className='max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center'>
-      <h2 className='text-2xl font-bold mb-4'>useRef Example</h2>
+      <h2 className='text-4xl font-semibold mt-4'>
+        ⏳ Timer: {time} sec
+      </h2>
+      <div className='flex gap-2 justify-center mt-2'>
+        <button
+          onClick={toggleTimer}
+          className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 cursor-pointer'
+        >
+          {isRunning ? 'Pause' : 'Start'}
+        </button>
+        <button
+          onClick={resetTimer}
+          className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer'
+        >
+          Reset
+        </button>
+      </div>
 
-      <input
-        type='text'
-        placeholder='Type something...'
-        className='w-full p-2 border rounded-lg'
-        ref={inputRef}
-        defaultValue='Hello World'
-      />
-
-      <button
-        className='mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
-        onClick={submit}
-      >
-        Submit
-      </button>
     </div>
   );
 };
