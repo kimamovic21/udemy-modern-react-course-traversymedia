8b20a5bc0938d6c7a358942b96d7e9fd586b3afe
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-06 15:59:57 +0200

    Product Context

--- shopping-cart-ui/src/components/ProductList.jsx
+++ shopping-cart-ui/src/components/ProductList.jsx
@@ -1,6 +1,9 @@
 import ProductCard from './ProductCard';
+import { useProducts } from '../../context/ProductContext';
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
+  const { products, loading, error } = useProducts();
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
       {products.map((product) => (
--- shopping-cart-ui/src/main.jsx
+++ shopping-cart-ui/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ProductProvider } from '../context/ProductContext.jsx';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ProductProvider>
+      <App />
+    </ProductProvider>
+  </StrictMode>
+);
