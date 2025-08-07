f3ed9353f9f08002185a07aabbeca7309058c26d
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-07 12:18:28 +0200

    Add to cart button

--- shopping-cart-ui/src/components/ProductCard.jsx
+++ shopping-cart-ui/src/components/ProductCard.jsx
@@ -1,4 +1,8 @@
+import { useCart } from '../../context/CartContext';
+
 const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
+
   return (
     <div
       key={product.id}
@@ -9,9 +13,25 @@ const ProductCard = ({ product }) => {
         alt={product.name}
         className='h-40 object-cover rounded mb-4'
       />
-      <h2 className='text-xl font-semibold'>{product.name}</h2>
-      <p className='text-gray-500 text-sm mb-2 h-10'>{product.description}</p>
-      <p className='font-bold text-lg'>${product.price.toFixed(2)}</p>
+
+      <h2 className='text-xl font-semibold'>
+        {product.name}
+      </h2>
+
+      <p className='text-gray-500 text-sm mb-2 h-10'>
+        {product.description}
+      </p>
+
+      <p className='font-bold text-lg'>
+        ${product.price.toFixed(2)}
+      </p>
+
+      <button
+        onClick={() => addToCart(product)}
+        className='bg-blue-600 text-white mt-3 px-4 py-2 rounded hover:bg-blue-700 transition'
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
