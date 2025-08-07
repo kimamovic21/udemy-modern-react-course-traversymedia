c7697bf85a208125872312f49e97f99249edd198
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-07 12:32:30 +0200

    Header & cart count

--- shopping-cart-ui/src/App.jsx
+++ shopping-cart-ui/src/App.jsx
@@ -1,13 +1,17 @@
 import ProductList from './components/ProductList';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-100 p-6'>
-      <h1 className='text-3xl font-bold mb-6'>
-        🛒 Product Catalog
-      </h1>
-      <ProductList />
-    </div>
+    <>
+      <Header />
+      <div className='min-h-screen bg-gray-100 p-6'>
+        <h1 className='mt-14 text-3xl font-bold mb-6'>
+          🛒 Product Catalog
+        </h1>
+        <ProductList />
+      </div>
+    </>
   );
 };
 
