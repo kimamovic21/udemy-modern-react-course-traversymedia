3944b892b6f22cd3ed28b45cd62d603f85588686
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-08 15:45:18 +0200

    Fetch projects with loader

--- friendly-dev/app/routes/projects/index.tsx
+++ friendly-dev/app/routes/projects/index.tsx
@@ -1,7 +1,21 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+export async function loader(
+  { request }: Route.LoaderArgs
+): Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+};
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return (
     <>
-      <h2 className='text-3xl font-bold mb-8 text-white'>
+      <h2 className='text-3xl text-white font-bold mb-8'>
         🚀 Projects
       </h2>
     </>
