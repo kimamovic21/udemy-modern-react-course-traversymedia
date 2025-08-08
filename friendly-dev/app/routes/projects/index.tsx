5f817db4c509cd901e898460e20b5d7af5e0fc5c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-08 16:40:39 +0200

    Display loader data

--- friendly-dev/app/routes/projects/index.tsx
+++ friendly-dev/app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
+import ProjectCard from '~/components/ProjectCard';
 
 export async function loader(
   { request }: Route.LoaderArgs
@@ -12,13 +13,18 @@ export async function loader(
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
 
   return (
     <>
-      <h2 className='text-3xl text-white font-bold mb-8'>
+      <h2 className='text-3xl font-bold mb-8 text-white'>
         🚀 Projects
       </h2>
+
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
