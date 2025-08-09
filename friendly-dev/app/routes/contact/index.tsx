c70db6dcec0afdced944331b22c66414b06a7ad4
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-09 15:31:26 +0200

    React router actions example

--- friendly-dev/app/routes/contact/index.tsx
+++ friendly-dev/app/routes/contact/index.tsx
@@ -1,11 +1,36 @@
-const ContactPage = () => {
+import { Form } from 'react-router';
+import type { Route } from './+types/index';
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get('name') as string;
+  const email = formData.get('email') as string;
+  const subject = formData.get('subject') as string;
+  const message = formData.get('message') as string;
+  const data = {
+    name,
+    email,
+    subject,
+    message,
+  };
+
+  return { message: 'Form submitted successfully!', data };
+};
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
   return (
     <section className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900'>
       <h2 className='text-3xl font-bold text-white mb-8 text-center'>
         📬 Contact Me
       </h2>
 
-      <form className='space-y-6'>
+      {actionData?.message ? (
+        <p className='mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-sm'>
+          {actionData.message}
+        </p>
+      ) : null}
+
+      <Form method='post' className='space-y-6'>
         <div>
           <label
             htmlFor='name'
@@ -68,11 +93,11 @@ const ContactPage = () => {
 
         <button
           type='submit'
-          className='w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition'
+          className='w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition cursor-pointer'
         >
           Send Message
         </button>
-      </form>
+      </Form>
     </section>
   );
 };
