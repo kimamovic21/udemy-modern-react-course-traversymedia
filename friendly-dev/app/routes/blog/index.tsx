042cc49af114a1e65e7a6a6a3c9c263e19d13604
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-11 14:12:28 +0200

    Fetch post meta

--- friendly-dev/app/routes/blog/index.tsx
+++ friendly-dev/app/routes/blog/index.tsx
@@ -1,4 +1,26 @@
-const BlogPage = () => {
+import { Link } from 'react-router';
+import type { Route } from './+types/index';
+import type { PostMeta } from '~/types';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+
+  const res = await fetch(url.href);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch posts');
+  };
+
+  const data = await res.json();
+
+  return { posts: data };
+};
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData as { posts: PostMeta[] };
+
   return (
     <>
       <h2 className='text-3xl font-bold mb-8 text-white'>
