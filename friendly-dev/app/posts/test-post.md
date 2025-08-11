6c0356332078abbe8255dc0eb5a37daf6566cdf2
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-11 14:58:27 +0200

    Display details page

---
+++ friendly-dev/app/posts/test-post.md
@@ -0,0 +1,5 @@
+# Test Post
+
+Random test post
+
+## Test Post Title
--- friendly-dev/app/routes/blog/details.tsx
+++ friendly-dev/app/routes/blog/details.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import type { Route } from './+types/details';
 import type { PostMeta } from '~/types';
 import ReactMarkdown from 'react-markdown';
@@ -39,12 +40,32 @@ const BlogPostDetailsPage = (
   { loaderData }: BlogPostDetailsPageProps
 ) => {
   const { postMeta, markdown } = loaderData;
-  console.log(postMeta, markdown);
 
   return (
-    <>
-      <h1>BlogPost</h1>
-    </>
+    <div className='max-w-3xl mx-auto px-6 py-12 bg-gray-900'>
+      <h1 className='text-3xl font-bold text-blue-400 mb-2'>
+        {postMeta.title}
+      </h1>
+
+      <p className='text-sm text-gray-400 mb-6'>
+        {new Date(postMeta.date).toLocaleDateString()}
+      </p>
+
+      <div className='prose prose-invert max-w-none mb-12'>
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
+      </div>
+
+      <div className='text-center'>
+        <Link
+          to='/blog'
+          className='inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition'
+        >
+          ← Go Back to Posts
+        </Link>
+      </div>
+    </div>
   );
 };
 
