8536df4b59b3b74f45fb774fd3df9998a69150db
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-14 12:47:47 +0200

    Formspree.io contact form

--- friendly-dev/friendly-dev-frontend/app/routes/contact/index.tsx
+++ friendly-dev/friendly-dev-frontend/app/routes/contact/index.tsx
@@ -1,59 +1,15 @@
-import { Form } from 'react-router';
-import type { Route } from './+types/index';
-
-export async function action({ request }: Route.ActionArgs) {
-  const formData = await request.formData();
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
-  const subject = formData.get('subject') as string;
-  const message = formData.get('message') as string;
-
-  const errors: Record<string, string> = {};
-
-  if (!name) errors.name = 'Name is required.';
-
-  if (!email) {
-    errors.email = 'Email is required.';
-  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    errors.email = 'Invalid email format.';
-  };
-
-  if (!subject) errors.subject = 'Subject is required.';
-
-  if (!message) {
-    errors.message = 'Message is required.';
-  };
-
-  if (Object.keys(errors).length > 0) {
-    return { errors };
-  };
-
-  const data = {
-    name,
-    email,
-    subject,
-    message,
-  };
-
-  return { message: 'Form submitted successfully!', data };
-};
-
-const ContactPage = ({ actionData }: Route.ComponentProps) => {
-  const errors = actionData?.errors || {};
-
+const ContactPage = () => {
   return (
     <section className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900'>
       <h2 className='text-3xl font-bold text-white mb-8 text-center'>
         📬 Contact Me
       </h2>
 
-      {actionData?.message ? (
-        <p className='mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-sm'>
-          {actionData.message}
-        </p>
-      ) : null}
-
-      <Form method='post' className='space-y-6'>
+      <form
+        method='post'
+        className='space-y-6'
+        action='https://formspree.io/f/xrbldprv'
+      >
         <div>
           <label
             htmlFor='name'
@@ -67,9 +23,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             name='name'
             className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
           />
-          {errors.name && (
-            <p className='text-red-400 text-sm mt-1'>{errors.name}</p>
-          )}
         </div>
 
         <div>
@@ -85,9 +38,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             name='email'
             className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
           />
-          {errors.email && (
-            <p className='text-red-400 text-sm mt-1'>{errors.email}</p>
-          )}
         </div>
 
         <div>
@@ -103,9 +53,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             name='subject'
             className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
           />
-          {errors.subject && (
-            <p className='text-red-400 text-sm mt-1'>{errors.subject}</p>
-          )}
         </div>
 
         <div>
@@ -121,9 +68,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             rows={5}
             className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
           />
-          {errors.message && (
-            <p className='text-red-400 text-sm mt-1'>{errors.message}</p>
-          )}
         </div>
 
         <button
@@ -132,7 +76,7 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
         >
           Send Message
         </button>
-      </Form>
+      </form>
     </section>
   );
 };
