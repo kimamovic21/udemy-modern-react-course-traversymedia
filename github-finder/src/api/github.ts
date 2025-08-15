a4ce64590d81d2bff2945557dea89adfcab33e44
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-15 16:48:51 +0200

    Show user suggestion

--- github-finder/src/api/github.ts
+++ github-finder/src/api/github.ts
@@ -9,3 +9,15 @@ export const fetchGitHubUser = async (username: string) => {
 
   return data;
 };
+
+export const searchGitHubUsers = async (query: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}
+  `);
+
+  if (!res.ok) throw new Error('Failed to search users');
+
+  const data = await res.json();
+
+  return data.items;
+};
--- github-finder/src/components/UserSearch.tsx
+++ github-finder/src/components/UserSearch.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { fetchGitHubUser } from '../api/github';
+import { useDebounce } from 'use-debounce';
+import { fetchGitHubUser, searchGitHubUsers } from '../api/github';
+import type { GitHubUser } from '../types';
 import UserCard from './UserCard';
 import RecentSearches from './RecentSearches';
 
@@ -11,11 +13,21 @@ const UserSearch = () => {
     const stored = localStorage.getItem('recentUsers');
     return stored ? JSON.parse(stored) : [];
   });
+  const [debouncedUsername] = useDebounce(username, 300);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-  const { data, isLoading, isError, error } = useQuery({
+  // Query to fetch specific user
+  const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ['users', submittedUsername],
     queryFn: () => fetchGitHubUser(submittedUsername),
     enabled: !!submittedUsername,
+  });
+
+  // Query to fetch suggestions for user search
+  const { data: suggestions } = useQuery({
+    queryKey: ['github-user-suggestions', debouncedUsername],
+    queryFn: () => searchGitHubUsers(debouncedUsername),
+    enabled: debouncedUsername.length > 1,
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -23,6 +35,8 @@ const UserSearch = () => {
     const trimmedUser = username.trim();
     if (!trimmedUser) return;
     setSubmittedUsername(trimmedUser);
+
+    setUsername('');
 
     setRecentUsers((prev) => {
       const updatedUser = [trimmedUser, ...prev.filter((user) => user !== trimmedUser)];
@@ -37,12 +51,55 @@ const UserSearch = () => {
   return (
     <>
       <form className='form' onSubmit={handleSubmit}>
-        <input
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder='Enter GitHub username'
-        />
+        <div className='dropdown-wrapper'>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => {
+              const val = e.target.value;
+              setUsername(val);
+              setShowSuggestions(val.trim().length > 1)
+            }}
+            placeholder='Enter GitHub username'
+          />
+          {showSuggestions && suggestions?.length > 0 && (
+            <ul className='suggestions'>
+              {suggestions.slice(0, 5).map((user: GitHubUser) => (
+                <li
+                  key={user.login}
+                  onClick={() => {
+                    setUsername(user.login);
+                    setShowSuggestions(false);
+
+                    if (submittedUsername !== user.login) {
+                      setSubmittedUsername(user.login);
+                    } else {
+                      refetch();
+                    };
+
+                    setRecentUsers((prev) => {
+                      const updated = [
+                        user.login,
+                        ...prev.filter((prevUsername) => prevUsername !== user.login),
+                      ];
+                      return updated.slice(0, 5);
+                    });
+                  }}
+                >
+                  <img
+                    src={user.avatar_url}
+                    alt={user.login}
+                    className='avatar-xs'
+                  />
+                  <span>
+                    {user.login}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
         <button type='submit'>Search</button>
       </form>
 
