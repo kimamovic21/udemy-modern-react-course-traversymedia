540f2847cf127e0302bbe37d47c5eb114dec9ca7
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-18 15:27:31 +0200

    Install & use Axios

--- idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
+++ idea-drop/idea-drop-ui/src/routes/ideas/$ideaId/index.tsx
@@ -1,13 +1,12 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import type { Idea } from '@/types';
+import api from '@/lib/axios';
 
 const fetchIdea = async (ideaId: string): Promise<Idea> => {
-  const res = await fetch(`/api/ideas/${ideaId}`);
+  const res = await api.get(`/ideas/${ideaId}`);
 
-  if (!res.ok) throw new Error('Failed to fetch data');
-
-  return res.json();
+  return res.data;
 };
 
 const ideaQueryOptions = (ideaId: string) =>
