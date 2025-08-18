c7e69222e8a8f4fc2bbe9b440013dd34ee26d367
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-18 15:58:05 +0200

    Not found page

--- idea-drop/idea-drop-ui/src/routes/__root.tsx
+++ idea-drop/idea-drop-ui/src/routes/__root.tsx
@@ -1,7 +1,8 @@
 import {
   Outlet,
   createRootRouteWithContext,
-  HeadContent
+  HeadContent,
+  Link
 } from '@tanstack/react-router';
 import { QueryClient } from '@tanstack/react-query';
 import { TanstackDevtools } from '@tanstack/react-devtools';
@@ -24,6 +25,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     ],
   }),
   component: RootLayout,
+  notFoundComponent: NotFoundPage,
 });
 
 function RootLayout() {
@@ -37,6 +39,27 @@ function RootLayout() {
         </div>
       </main>
       <TanstackDevtools />
+    </div>
+  );
+};
+
+function NotFoundPage() {
+  return (
+    <div className='flex flex-col items-center justify-center text-center py-20'>
+      <h1 className='text-4xl font-bold text-gray-800 mb-4'>
+        404
+      </h1>
+
+      <p className='text-lg text-gray-600 mb-6'>
+        Oops! The page you're looking for doesn't exist.
+      </p>
+
+      <Link
+        to='/'
+        className='px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition'
+      >
+        Go back home
+      </Link>
     </div>
   );
 };
