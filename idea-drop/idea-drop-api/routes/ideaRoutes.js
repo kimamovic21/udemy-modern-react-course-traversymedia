08aa20d3be039f07406c64166ed4d768de7a0ea6
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-20 15:40:26 +0200

    Model & fetch ideas

--- idea-drop/idea-drop-api/routes/ideaRoutes.js
+++ idea-drop/idea-drop-api/routes/ideaRoutes.js
@@ -1,18 +1,45 @@
 import express from 'express';
+import Idea from '../models/Idea.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
 // @route GET     /api/ideas
 // @description   Get all ideas
 // @access        Public
-router.get('/', (req, res) => {
-  const ideas = [
-    { id: 1, title: 'Idea 1', description: 'Description for Idea 1' },
-    { id: 2, title: 'Idea 2', description: 'Description for Idea 2' },
-    { id: 3, title: 'Idea 3', description: 'Description for Idea 3' },
-  ];
+router.get('/', async (req, res, next) => {
+  try {
+    const ideas = await Idea.find().sort({ createdAt: -1 });
 
-  res.json(ideas);
+    res.json(ideas);
+  } catch (err) {
+    next(err);
+  };
+});
+
+// @route GET     /api/ideas/:id
+// @description   Get a single idea by ID
+// @access        Public
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error('Idea not found');
+    };
+
+    const idea = await Idea.findById(id);
+
+    if (!idea) {
+      res.status(404);
+      throw new Error('Idea not found');
+    };
+
+    res.json(idea);
+  } catch (err) {
+    next(err);
+  };
 });
 
 // @route POST    /api/ideas
