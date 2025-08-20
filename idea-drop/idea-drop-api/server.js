99ab9b85df1182a02f82608262d49ddffc21d5f5
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-20 14:00:23 +0200

    Route files

--- idea-drop/idea-drop-api/server.js
+++ idea-drop/idea-drop-api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import ideasRouter from './routes/ideaRoutes.js';
 
 dotenv.config();
 
@@ -11,23 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/ideas', (req, res) => {
-  const ideas = [
-    { id: 1, title: 'Idea 1', description: 'Description for Idea 1' },
-    { id: 2, title: 'Idea 2', description: 'Description for Idea 2' },
-    { id: 3, title: 'Idea 3', description: 'Description for Idea 3' },
-  ];
-
-  res.json(ideas);
-});
-
-app.post('/api/ideas', (req, res) => {
-  const { title, description } = req.body || {};
-
-  console.log(title, description);
-
-  res.send(title);
-});
+app.use('/api/ideas', ideasRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
