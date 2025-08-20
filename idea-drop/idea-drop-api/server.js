5213c0ea08a25a97637cd2de29f189b36e2f7904
Kerim Imamović <imamovic.kerim@gmail.com>
2025-08-20 14:17:31 +0200

    Custom error handler

--- idea-drop/idea-drop-api/server.js
+++ idea-drop/idea-drop-api/server.js
@@ -1,3 +1,4 @@
+import { errorHandler } from './middleware/errorHandler.js';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -6,13 +7,28 @@ import ideasRouter from './routes/ideaRoutes.js';
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use('/api/ideas', ideasRouter);
+
+// 404 Fallback
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.get('/error', (req, res) => {
+  throw new Error('This is a test error!');
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
